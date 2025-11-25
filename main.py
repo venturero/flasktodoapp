94cc48171909432014fef54def03e882eaa60e32
semiventurero <63498905+venturero@users.noreply.github.com>
2025-11-25 13:52:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -292,6 +292,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    port = int(os.environ.get('PORT', 5000))
+    #port = int(os.environ.get('PORT', 5000))
 
-    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=port)
